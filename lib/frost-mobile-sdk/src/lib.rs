0c34b67e80955afa4f66b1a9756be5087876bdb3
Francisco Gindre <francisco.gindre@gmail.com>
2024-05-03 19:31:25 -0300

    basic SecretKey Package and PublicKey Package

--- lib/frost-mobile-sdk/src/lib.rs
+++ lib/frost-mobile-sdk/src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-
+use thiserror::Error;
 use reddsa::frost::redpallas as frost;
 use uniffi;
 use frost::keys::PublicKeyPackage;
@@ -14,6 +14,13 @@ pub struct Header {
     pub version: u8, 
     pub suite: Ciphersuite, 
 }
+#[derive(uniffi::Record)]
+pub struct FrostKeyShare {
+    pub header: Header,
+    pub identifier: String,
+    pub signing_share: String,
+    pub commitment: Vec<String>,
+}
 
 #[derive(uniffi::Record)]
 pub struct FrostPublicKeyPackage {
@@ -24,6 +31,44 @@ pub struct FrostPublicKeyPackage {
 
 impl FrostPublicKeyPackage {
    
+}
+
+#[derive(uniffi::Record)]
+pub struct Configuration {
+    pub min_signers: u16,
+    pub max_signers: u16,
+    pub secret: Vec<u8>,
+}
+
+#[derive(Debug, uniffi::Error, thiserror::Error)]
+pub enum ConfigurationError {
+    #[error("Number of maximum signers in invalid.")]
+    InvalidMaxSigners,
+    #[error("Number of minimum signers in invalid.")]
+    InvalidMinSigners,
+    #[error("The Secret can't be empty")]
+    InvalidEmptySecret,
+}
+
+#[uniffi::export]
+fn validate_config(config: &Configuration) -> Result<(), ConfigurationError> {
+    if config.min_signers < 2 {
+        return Err(ConfigurationError::InvalidMinSigners);
+    }
+
+    if config.max_signers < 2 {
+        return Err(ConfigurationError::InvalidMaxSigners);
+    }
+
+    if config.min_signers > config.max_signers {
+        return Err(ConfigurationError::InvalidMinSigners);
+    }
+
+    if config.secret.is_empty() {
+        return Err(ConfigurationError::InvalidEmptySecret)
+    }
+    
+    Ok(())
 }
 
 #[derive(uniffi::Object)]
