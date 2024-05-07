594b8f2daa11ed19ca39a7d45400e4a86ffddbf5
Francisco Gindre <francisco.gindre@gmail.com>
2024-05-07 17:12:27 -0300

    FrostSecretShare to frost::keys::SecretShare

--- lib/frost-mobile-sdk/src/lib.rs
+++ lib/frost-mobile-sdk/src/lib.rs
@@ -1,5 +1,11 @@
+use frost::keys::SigningShare;
+use frost::Ciphersuite;
+use frost::Identifier;
 #[cfg(not(feature = "redpallas"))]
 use frost_ed25519 as frost;
+use reddsa::frost::redpallas::frost::keys::CoefficientCommitment;
+use reddsa::frost::redpallas::frost::keys::VerifiableSecretSharingCommitment;
+use reddsa::frost::redpallas::frost::Identifier;
 #[cfg(feature = "redpallas")]
 use reddsa::frost::redpallas as frost;
 
@@ -12,15 +18,7 @@ use frost::keys::SecretShare;
 use frost::keys::PublicKeyPackage;
 uniffi::setup_scaffolding!();
 
-#[derive(uniffi::Enum)]
-pub enum Ciphersuite {
-    RedPallas
-}
-#[derive(uniffi::Record)]
-pub struct Header {
-    pub version: u8, 
-    pub suite: Ciphersuite, 
-}
+
 #[derive(uniffi::Record)]
 pub struct FrostSecretKeyShare {
     pub identifier: String,
@@ -39,17 +37,53 @@ impl FrostSecretKeyShare {
         let identifier = secret_share.identifier();
         let signing_share = secret_share.signing_share();
         let commitment = secret_share.commitment()
-        .coefficients()
-        .iter()
-        .map(|c| hex::encode(c.serialize()))
-        .collect();
-
-
+            .serialize()
+            .into_iter()
+            .map(|c| hex::encode(c))
+            .collect();
         FrostSecretKeyShare {
             identifier: hex::encode(identifier.serialize()),
             signing_share: hex::encode(signing_share.serialize()),
             commitment: commitment,
         }
+    }
+
+    fn to_secret_share(&self) -> Result<SecretShare, Error> {
+        let hex_identifier = hex::decode(&mut self.identifier)
+            .map_err(|_| frost::Error::DeserializationError)?;
+        
+        let slice_identifier = hex_identifier[0..32]
+            .try_into()
+            .map_err(|_| frost::Error::DeserializationError)?;
+
+        let identifier = Identifier::deserialize(slice_identifier)?;
+
+        let hex_signing_share = hex::decode(self.signing_share)
+        .map_err(|_| frost::Error::DeserializationError)?;
+
+        let signing_share = hex_signing_share[0..32]
+            .try_into()
+            .map_err(|_| frost::Error::DeserializationError)
+            .map(|v| 
+                SigningShare::deserialize(v)
+                    .map_err(|_| frost::Error::DeserializationError)?
+                )?;
+
+        for s in self.commitment {
+            let hex_commitment = hex::decode(s)
+                    .map_err(|_| frost::Error::SerializationError)?;
+        }
+        let v = self.commitment
+            .into_iter()
+            .map(|s| 
+                
+            )
+            .collect();
+
+    
+        let secret_share = SecretShare::new(identifier, signing_share, commitments);
+
+        Ok(secret_share)
     }
 }
 
